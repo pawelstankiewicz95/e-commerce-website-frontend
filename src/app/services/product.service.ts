b6c4b6f0b6b2f2fb62dfb95f8a4107635c0560b9
pawelstankiewicz <pawelsta95@gmail.com>
2023-03-10 21:42:49 +0100

    feat: create logic for ProductService

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,9 +1,30 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Product } from '../common/product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  private apiServerUrl = 'http://localhost:8082/api/products';
+
+  constructor(private httpClient: HttpClient) { }
+
+  public getProucts(): Observable<Product[]> {
+    return this.httpClient.get<Product[]>(this.apiServerUrl);
+  }
+
+  public saveProduct(product: Product): Observable<Product> {
+    return this.httpClient.post<Product>(this.apiServerUrl, product);
+  }
+
+  public updateProduct(product: Product): Observable<Product> {
+    return this.httpClient.put<Product>(this.apiServerUrl, product);
+  }
+
+  public deleteProduct(productID: number): Observable<void> {
+    return this.httpClient.delete<void>('${this.apiServerUrl}/${productID}');
+  }
 }
