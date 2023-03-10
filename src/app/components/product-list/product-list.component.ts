0934c00b7f846044e05cf98b474cac5be155c306
pawelstankiewicz <pawelsta95@gmail.com>
2023-03-10 23:14:11 +0100

    feat: create getProducts method

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/common/product';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
+
+  public products: Product[] = [];
+
+  constructor(private productService: ProductService) { }
+
+  ngOnInit(): void {
+    this.getProducts();
+  }
+
+  public getProducts(): void {
+    this.productService.getProucts().subscribe((response: Product[]) => this.products = response);
+  }
 
 }
