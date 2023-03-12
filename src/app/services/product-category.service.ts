653b84ab0d00bf39c54a9e17ee3f2f853682720b
pawelstankiewicz <pawelsta95@gmail.com>
2023-03-13 00:02:03 +0100

    feat: create ProductCategory service

---
+++ src/app/services/product-category.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { ProductCategory } from '../common/product-category';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductCategoryService {
+  private apiServerUrl = 'http://localhost:8082/api/product-categories';
+
+  constructor(private httpClient: HttpClient) { }
+
+  public getProductCategories(): Observable<ProductCategory[]> {
+    return this.httpClient.get<ProductCategory[]>(this.apiServerUrl);
+  }
+}
