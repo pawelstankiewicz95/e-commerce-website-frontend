5a912205238597d452d7f428e2d892738492e3ef
pawelstankiewicz <pawelsta95@gmail.com>
2023-03-13 00:31:50 +0100

    feat:create getProductCategories, ngOnInit methods

--- src/app/components/product-category-menu/product-category-menu.component.ts
+++ src/app/components/product-category-menu/product-category-menu.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductCategory } from 'src/app/common/product-category';
+import { ProductCategoryService } from 'src/app/services/product-category.service';
 
 @Component({
   selector: 'app-product-category-menu',
@@ -6,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-category-menu.component.css']
 })
 export class ProductCategoryMenuComponent {
+
+  public productCategories: ProductCategory[] = [];
+
+  constructor(private productCategoryService: ProductCategoryService) { }
+
+  ngOnInit(): void {
+    this.getProductCategories();
+  }
+
+  public getProductCategories(): void {
+    this.productCategoryService.getProductCategories().subscribe((response: ProductCategory[]) => this.productCategories = response);
+  }
 
 }
