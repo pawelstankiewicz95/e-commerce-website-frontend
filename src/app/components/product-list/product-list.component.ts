33740382dafaee27f56bf35624ba957fd549a1f0
pawelstankiewicz <pawelsta95@gmail.com>
2023-03-14 01:39:20 +0100

    add getting products by catergory functionallity

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Product } from 'src/app/common/product';
 import { ProductService } from 'src/app/services/product.service';
 
@@ -11,14 +12,32 @@ export class ProductListComponent implements OnInit {
 
   public products: Product[] = [];
 
-  constructor(private productService: ProductService) { }
+  public productsByCategoryId: Product[] = [];
+
+  categoryId: number = 1;
+
+  constructor(private productService: ProductService,
+    private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.getId();
     this.getProducts();
+    this.getProductsByCategoryId(this.categoryId);
   }
 
   public getProducts(): void {
     this.productService.getProucts().subscribe((response: Product[]) => this.products = response);
+  }
+
+  public getProductsByCategoryId(id: number) {
+    this.productService.getProductsByCategoryId(id).subscribe((response: Product[]) => this.productsByCategoryId = response);
+  }
+
+  public getId():void{
+    const hasCategoryId = this.route.snapshot.paramMap.has('id');
+    if (hasCategoryId) {
+      this.categoryId = +this.route.snapshot.paramMap.get('id')!;
+    }
   }
 
 }
