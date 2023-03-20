76cff51873cf95949b0668d501cbed0346b46c96
pawelstankiewicz <pawelsta95@gmail.com>
2023-03-21 00:05:52 +0100

    feat: create product details component

--- src/app/components/product-details/product-details.component.ts
+++ src/app/components/product-details/product-details.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from 'src/app/common/product';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product-details',
@@ -7,4 +10,28 @@ import { Component } from '@angular/core';
 })
 export class ProductDetailsComponent {
 
+  public product!: Product;
+  productId: number = 1;
+  hasProductId: boolean = false;
+
+  constructor(private productService: ProductService,
+    private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.route.paramMap.subscribe(() => this.handleShowingProductDetails());
+  }
+
+  public handleShowingProductDetails() {
+    this.hasProductId = this.route.snapshot.paramMap.has('productId');
+    if (this.hasProductId) {
+      this.getProductById();
+    }
+  }
+
+  public getProductById() {
+    this.productId = +this.route.snapshot.paramMap.get('productId')!;
+    this.productService.getProductById(this.productId).subscribe((response: Product) => this.product = response);
+  }
+
 }
+
