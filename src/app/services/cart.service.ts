44c0acd02e9d9465b732fa9f1e6973492603754b
pawelstankiewicz <pawelsta95@gmail.com>
2023-03-23 00:36:38 +0100

    feat: add advanced cart service logic add function for computing cart content; handle duplicates in cart array

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -1,22 +1,54 @@
 import { Injectable } from '@angular/core';
+import { CartProduct } from '../common/cart-product';
 import { Product } from '../common/product';
+import { Subject } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
-  products: Product[] = [];
+
+  totalCartValue: Subject<number> = new Subject<number>();
+  totalQuantityOfProducts: Subject<number> = new Subject<number>();
+
+  cartProducts: CartProduct[] = [];
+
   constructor() { }
+
   addToCart(product: Product) {
-    this.products.push(product);
+    let cartProduct: CartProduct = new CartProduct(product);
+    let productInCart = undefined;
+    if (this.cartProducts.length > 0) {
+      productInCart = this.cartProducts.find(tempCartProduct => tempCartProduct.product.id === product.id);
+
+    }
+    if (productInCart != undefined) {
+      productInCart.quantity++;
+    } else {
+      this.cartProducts.push(cartProduct);
+    }
+
+    this.computeCartContent();
   }
 
   getItems() {
-    return this.products;
+    return this.cartProducts;
   }
 
   clearCart() {
-    this.products = [];
-    return this.products;
+    this.cartProducts = [];
+    return this.cartProducts;
+  }
+
+  computeCartContent() {
+    let computedTotalCartValue = 0;
+    let computedTotalQuanity = 0;
+    for (let tempCartProduct of this.cartProducts) {
+      computedTotalCartValue += tempCartProduct.product.unitPrice * tempCartProduct.quantity;
+      computedTotalQuanity += tempCartProduct.quantity;
+    }
+    this.totalCartValue.next(computedTotalCartValue);
+    this.totalQuantityOfProducts.next(computedTotalQuanity);
   }
 }
