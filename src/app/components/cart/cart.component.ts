176092130dc5e581556177be7bf6662c3b84d32d
pawelstankiewicz <pawelsta95@gmail.com>
2023-03-24 00:37:16 +0100

    feat: create cart view w/o summary view

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { CartProduct } from 'src/app/common/cart-product';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-cart',
@@ -6,5 +8,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent {
+  cartProducts: CartProduct[] = [];
+  totalCartValue: number = 0;
+  totalQuantityOfProducts: number = 0;
+
+  constructor(private cartService: CartService) { };
+
+  ngOnInit() {
+    this.showCart();
+  }
+
+  showCart() {
+    this.cartProducts = this.cartService.cartProducts;
+    this.cartService.totalCartValue.subscribe((response: number) => this.totalCartValue = response);
+    this.cartService.totalQuantityOfProducts.subscribe((response: number) => this.totalQuantityOfProducts = response);
+    this.cartService.computeCartContent();
+  }
+
+
+
 
 }
