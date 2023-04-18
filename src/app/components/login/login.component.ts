86f0342f4fabccb17b467a02a704d3a2b230dbe6
pawelstankiewicz <pawelsta95@gmail.com>
2023-04-19 00:16:16 +0200

    feat: create functionality for login component

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { OKTA_AUTH } from '@okta/okta-angular';
+import OktaAuth from '@okta/okta-auth-js';
+import OktaSignIn from '@okta/okta-signin-widget';
+import appConfig from 'src/app/config/app-config';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+
+  oktaSignIn: any;
+
+  constructor(@Inject(OKTA_AUTH) private oktaAuth: OktaAuth) {
+    this.oktaSignIn = new OktaSignIn({
+      logo: '',
+      baseUrl: appConfig.oidc.issuer.split('/oauth2')[0],
+      clientId: appConfig.oidc.clientId,
+      redirectUri: appConfig.oidc.redirectUri,
+      authParams: {
+        issuer: appConfig.oidc.issuer,
+        scopes: appConfig.oidc.scopes
+      }
+    });
+  }
+
+  ngOnInit(): void {
+    this.oktaSignIn.remove;
+
+    this.oktaSignIn.renderEl({
+      el: '#okta-sign-in-widget'
+    },
+      (response: any) => {
+        if (response.status === 'success') {
+          this.oktaAuth.signInWithRedirect();
+        }
+      },
+      (error: any) => { throw error;}
+    )
+
+
+
+  }
 
 }
