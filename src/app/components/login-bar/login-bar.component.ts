07d7decc30d3e757de2215fab93e63b72ae760a8
pawelstankiewicz <pawelsta95@gmail.com>
2023-05-02 00:49:01 +0200

    feat: handle saving cart to db after logout

--- src/app/components/login-bar/login-bar.component.ts
+++ src/app/components/login-bar/login-bar.component.ts
@@ -1,6 +1,11 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { OKTA_AUTH, OktaAuthStateService } from '@okta/okta-angular';
-import {OktaAuth} from '@okta/okta-auth-js';
+import { OktaAuth } from '@okta/okta-auth-js';
+import { firstValueFrom, lastValueFrom } from 'rxjs';
+import { Cart } from 'src/app/common/cart';
+import { CartProduct } from 'src/app/common/cart-product';
+import { User } from 'src/app/common/user';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-login-bar',
@@ -11,8 +16,11 @@ export class LoginBarComponent implements OnInit {
 
   isAuthenticated: boolean = false;
   userName: string = '';
+  userEmail: string = '';
+  cartProducts: CartProduct[] = [];
+  storage: Storage = localStorage;
 
-  constructor(private oktaAuthService: OktaAuthStateService, @Inject(OKTA_AUTH) private oktaAuth: OktaAuth) {
+  constructor(private oktaAuthService: OktaAuthStateService, @Inject(OKTA_AUTH) private oktaAuth: OktaAuth, private cartService: CartService) {
 
   }
 
@@ -26,10 +34,26 @@ export class LoginBarComponent implements OnInit {
   getUserDetails(): void {
     if (this.isAuthenticated) {
       this.oktaAuth.getUser().then((result) => this.userName = result.name as string)
+      this.oktaAuth.getUser().then((result) => this.userEmail = result.email as string);
     }
   }
 
-  logout(): void {
-      this.oktaAuth.signOut();
+  saveCart() {
+    this.cartProducts = this.cartService.cartProducts;
+    this.cartService.computeCartContent();
+    let user: User = new User();
+    user.email = this.userEmail;
+    let cart: Cart = new Cart(user, this.cartProducts);
+    console.log(cart);
+    return this.cartService.saveCart(cart);
+  }
+
+  async logout() {
+    try {
+      await lastValueFrom(this.saveCart());
+    } catch (e) {
+      console.log(e);
+    }
+    await this.oktaAuth.signOut();
   }
 }
