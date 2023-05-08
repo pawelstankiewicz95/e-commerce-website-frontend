e788aba9f346f7273c70d24d5f44599ab2e2cb75
pawelstankiewicz <pawelsta95@gmail.com>
2023-05-08 21:15:04 +0200

    feat: add auth-interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
-import { HttpClientModule } from '@angular/common/http'
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http'
 import { ProductService } from './services/product.service';
 import { ProductCategoryMenuComponent } from './components/product-category-menu/product-category-menu.component';
 import { RouterModule, Routes } from '@angular/router';
@@ -28,6 +28,7 @@ import { OktaAuth } from '@okta/okta-auth-js';
 
 import appConfig from './config/app-config';
 import { CustomLoingCallbackComponent } from './components/custom-login-callback-component/custom-login-callback.component';
+import { AuthInterceptor } from './services/auth-interceptor.service';
 
 const oktaConfig = appConfig.oidc;
 
@@ -70,7 +71,7 @@ const routes: Routes = [
     ReactiveFormsModule,
     OktaAuthModule
   ],
-  providers: [ProductService, { provide: OKTA_CONFIG, useValue: { oktaAuth } }],
+  providers: [ProductService, { provide: OKTA_CONFIG, useValue: { oktaAuth } }, { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
