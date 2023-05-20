9333e3c9d19f2f9ba2d23cb31db3d6623921fadf
pawelstankiewicz <pawelsta95@gmail.com>
2023-05-20 21:25:43 +0200

    feat: add product-category-update service for live updating component

--- src/app/components/product-category-menu/product-category-menu.component.ts
+++ src/app/components/product-category-menu/product-category-menu.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductCategory } from 'src/app/common/product-category';
+import { ProductCategoryUpdateService } from 'src/app/services/product-category-update.service';
 import { ProductCategoryService } from 'src/app/services/product-category.service';
 
 @Component({
@@ -11,10 +12,16 @@ export class ProductCategoryMenuComponent {
 
   public productCategories: ProductCategory[] = [];
 
-  constructor(private productCategoryService: ProductCategoryService) { }
+  constructor(private productCategoryService: ProductCategoryService, private productCategoryUpdateService: ProductCategoryUpdateService) { }
 
   ngOnInit(): void {
     this.getProductCategories();
+    this.productCategoryUpdateService.productCategoriesUpdated$.subscribe(
+      (productCategories: ProductCategory[]) => {
+        // Update the component with the new product categories
+        this.productCategories = productCategories;
+      }
+    );
   }
 
   public getProductCategories(): void {
